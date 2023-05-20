30cc36ebfab93424ccb1409b889c71e711515441
Lucas Matias Gomez <lucasmatias@vivaldi.net>
2023-05-19 21:36:35 -0300

    Agregando peticición al para el usuario

--- challengeIV.py
+++ challengeIV.py
@@ -1,6 +1,8 @@
+inmueble_a_verificar = input('Ingrese datos del inmueble respetando el siguiente formato {"año": año_del_inmueble, "metros": metros_cuadrados, "habitaciones": cantidad_de_habitaciones, "garaje": si_no, "zona": A_B_C, "estado": Disponible_Reservado_Vendido}')
+
 inmuebles_validados = []
 
 def inmueble_check(año, metros, habitaciones, garaje, zona, estado):
     if zona in ['A', 'B', 'C'] and estado in ['Disponible', 'Reservado', 'Vendido']:
-        if año > 1999 and metros >= 60  and habitaciones > 1:
+        if año > 1999 and metros >= 60  and habitaciones >= 2:
             nuevo_inmueble = {}
