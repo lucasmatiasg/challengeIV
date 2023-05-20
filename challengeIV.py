da0f672db3f0912409c78c101830e536da93d534
Lucas Matias Gomez <lucasmatias@vivaldi.net>
2023-05-19 23:03:58 -0300

    Sacando la petición inicial al usuario

--- challengeIV.py
+++ challengeIV.py
@@ -1,8 +1,10 @@
-inmueble_a_verificar = input('Ingrese datos del inmueble respetando el siguiente formato {"año": año_del_inmueble, "metros": metros_cuadrados, "habitaciones": cantidad_de_habitaciones, "garaje": si_no, "zona": A_B_C, "estado": Disponible_Reservado_Vendido}')
-
 inmuebles_validados = []
 
 def inmueble_check(año, metros, habitaciones, garaje, zona, estado):
     if zona in ['A', 'B', 'C'] and estado in ['Disponible', 'Reservado', 'Vendido']:
         if año > 1999 and metros >= 60  and habitaciones >= 2:
-            nuevo_inmueble = {}
+            nuevo_inmueble = {'año': año, 'metros': metros, 'habitaciones': habitaciones, 'garaje': garaje, 'zona':                                 zona, 'estado': estado}
+            inmuebles_validados.append(nuevo_inmueble)
+            print(inmuebles_validados)
+        else:
+            print('El inmueble no cumple con los requisitos por lo que no va a ser tenido en cuenta, Gracias')
